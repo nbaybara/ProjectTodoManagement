b68855ae82fe0788b2695a48916835877f604ca2
nbaybara <nsbaybar@gmail.com>
2020-10-09 00:19:27 +0300

    App Routing Modules completed

--- src/webui/src/app/pages/project/project.component.ts
+++ src/webui/src/app/pages/project/project.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ProjectComponent implements OnInit {
 
   constructor() { }
-
+  projectMessage = 'Project Message';
   ngOnInit(): void {
   }
 
